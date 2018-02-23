344b90cdfb2e83d2bd1561c66874aa86b55287a3
Prasad <prasad.masurkar@capgemini.com>
2018-02-23 17:18:41 +0530

    updated test case for new validation conditions tests.

--- DemoProject/src/test1.java
+++ DemoProject/src/test1.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class test1 {
 	
@@ -39,5 +39,26 @@ public class test1 {
 			while (y<=3) {
 			System.out.println("Print the value "+y);
 			y++;}
+			
+			int a,b;
+			Scanner sc=new Scanner(System.in);
+			
+			System.out.println("Enter 1st number as A");
+			a= sc.nextInt();
+			System.out.println("Enter 2nd number as B");
+			b= sc.nextInt();
+			
+					//Compare 2 numbers entered and display largest of these 2 numbers.
+			
+			if (a>b)
+			{
+				System.out.println("Largest number amongst both numbers is A " +a);
+			}
+			else 
+			{
+				System.out.println("Largest number amongst both numbers is B " +b);
+			}
+			
+			sc.close();
 }
 }
